c35a71589c8c1ce53d3e78a83ce17d4088a692b3
Carlos Martin Rodriguez <crodriguez@baufest.com>
2016-12-21 15:48:15 -0300

    tp-clase-1

---
+++ tp_clase_1.js
@@ -0,0 +1,14 @@
+//Pedir un numero y sumarlo a otro que este en una variable
+var number1= 20;
+var number2 = parseInt(prompt('ingrese un numero'));
+var number= number1 + number2;
+
+//Pedir un numero y restarlo a otro que este en una variable
+var number1= 200;
+var number2= parseInt(prompt('ingrese un numero'));
+var number= number1 - number2;
+
+//Pedir un numero, luego otro y ambos sumarlos
+var number1= parseInt(prompt('ingrese un numero'))
+var number2= parseInt(prompt('ingrese otro numero'))
+var number= number1 + number2;
