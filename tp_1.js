7a44a119dcceda6dfc20509cc96ccdb6b6132b27
Carlos Martin Rodriguez <crodriguez@baufest.com>
2016-12-21 15:15:21 -0300

    primer

---
+++ tp_1.js
@@ -0,0 +1,29 @@
+var diaSemana = prompt('Ingrese dia de la semana');
+
+switch (diaSemana) {
+
+	case 'lunes':
+			alert('El dia es habil');
+			break;
+	case 'martes':
+			alert('El dia es habil');
+			break;
+	case 'miercoles':
+			alert('El dia es habil');
+			break;
+    case 'jueves':
+			alert('El dia es habil');
+			break;
+    case 'viernes':
+			alert('El dia es habil');
+			break;
+    case 'sabado':
+			alert('El dia es fin de semana');
+			break;
+    case 'domingo':
+			alert('El dia es fin de semana');
+			break;    
+    default:
+			alert('Hoy no es ningun dia!!');
+
+}
